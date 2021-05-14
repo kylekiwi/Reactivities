43e6689c6e4233896f0d06b909cc8172d1eb2d3f
Kyle <hijongwon@gmail.com>
2021-05-14 18:18:01 +0900

    ...

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
 import { Grid } from 'semantic-ui-react';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+import { useStore } from '../../app/stores/store';
 import ProfileContent from './ProfileContent';
 import ProfileHeader from './ProfileHeader';
 
 const ProfilePage = () => {
+  const {username} = useParams<{username: string}>();
+  const {profileStore} = useStore();
+  const {loadingProfile, loadProfile, profile} = profileStore;
+  
+  useEffect(() => {
+    loadProfile(username);
+  }, [loadProfile, username])
+
+  if(loadingProfile) return <LoadingComponent content='Loading profile...' />
+  
   return (
     <Grid>
       <Grid.Column width={16}>
-        <ProfileHeader />
-        <ProfileContent />
+        {profile &&
+        <>
+          <ProfileHeader profile={profile} /> 
+          <ProfileContent profile={profile} />
+        </>}
       </Grid.Column>
     </Grid>
   );
 };
 
-export default ProfilePage;
+export default observer(ProfilePage);
