4e687264100353751c7780f923355c5ee05cc333
Kyle <hijongwon@gmail.com>
2021-05-15 15:31:27 +0900

    ...

---
+++ Application/Comments/Create.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Inferfaces;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Comment
+{
+  public class Create
+  {
+    public class Command : IRequest<Result<CommentDto>>
+    {
+      public string Body { get; set; }
+      public Guid ActivityId { get; set; }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+      public CommandValidator()
+      {
+        RuleFor(x => x.Body).NotEmpty();
+      }
+    }
+
+    public class Hander : IRequestHandler<Command, Result<CommentDto>>
+    {
+      private readonly IUserAccessor _userAccessor;
+      private readonly IMapper _mapper;
+      private readonly DataContext _context;
+      public Hander(DataContext context, IMapper mapper, IUserAccessor userAccessor)
+      {
+        _context = context;
+        _mapper = mapper;
+        _userAccessor = userAccessor;
+      }
+
+      public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
+      {
+        var activity = await _context.Activities.FindAsync(request.ActivityId)
+        if(activity == null) return null;
+        var user = await _context.Users
+          .Include(p => p.Photos)
+          .SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+        var comment = new Comment
+        {
+          Author = user,
+          Activity = activity,
+          Body = request.Body
+        };
+        activity.Comments.Add(comment);
+        var success = await _context.SaveChangesAsync() > 0;
+        if(success) return Result<CommentDto>.Success(_mapper.Map<CommentDto>(comment));
+        return Result<CommentDto>.Failure("Failed to add comment");
+      }
+    }
+
+  }
+}
