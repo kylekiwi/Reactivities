5cdf82f8ae1c5729c28a53c5ab5bd7871f9db714
Kyle <hijongwon@gmail.com>
2021-05-17 08:27:20 +0900

    ...

---
+++ client-app/src/app/layout/PrivateRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Redirect, RouteComponentProps, RouteProps } from 'react-router';
+import { Route } from 'react-router-dom';
+import { useStore } from '../stores/store';
+
+interface Props extends RouteProps {
+  component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+}
+
+const PrivateRoute = ({component: Component, ...rest}: Props) => {
+  const {userStore: {isLoggedIn}} = useStore();
+  return (
+    <Route 
+      {...rest}
+      render={(props) => isLoggedIn ? <Component {...props} /> : <Redirect to='/' /> }
+    />
+  );
+};
+
+export default PrivateRoute;
