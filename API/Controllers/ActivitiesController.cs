98270efd00390b0887e1e6835c22cadd8c91fae9
Kyle <hijongwon@gmail.com>
2021-05-16 22:11:58 +0900

    ...

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -16,24 +16,21 @@ namespace API.Controllers
 	{
 
 		[HttpGet]
-		public async Task<IActionResult> GetActivities([FromQuery]PagingParams param)
+		public async Task<IActionResult> GetActivities([FromQuery]ActivityParams param)
 		{
 			// return HandleResult(await Mediator.Send(new List.Query{Params=param}));
 			return HandlePagedResult(await Mediator.Send(new List.Query{Params=param}));
 		}
 
 		[HttpGet("{id}")]
-		// public async Task<ActionResult<Activity>> GetActivity(Guid id)
 		public async Task<IActionResult> GetActivity(Guid id)
 		{
-			// return await Mediator.Send(new Details.Query{Id=id});
 			return HandleResult(await Mediator.Send(new Details.Query{Id=id}));
 		}
 		
 		[HttpPost]
 		public async Task<IActionResult> CreateActivity(Activity activity)
 		{
-			// return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
 			return HandleResult(await Mediator.Send(new Create.Command{Activity = activity}));
 		}
 		
@@ -49,7 +46,6 @@ namespace API.Controllers
 		[HttpDelete("{id}")]
 		public async Task<IActionResult> DeleteActivity(Guid id)
 		{
-			// return Ok(await Mediator.Send(new Delete.Command{Id=id}));
 			return HandleResult(await Mediator.Send(new Delete.Command{Id=id}));
 		}
 		
